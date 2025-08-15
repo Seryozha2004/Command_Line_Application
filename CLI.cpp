7dba9af66ad26e211f9823dd7f9170d649449196
Seryozha2004 <kirakosyans860@gmail.com>
2025-08-15 06:41:08 +0000

    Updated version with some checkings

--- CLI.cpp
+++ CLI.cpp
@@ -17,6 +17,8 @@ public:
 	virtual ~WindowBase() {}
 	string getId() const { return id; }
 	WindowBase* getParent() const { return parent; }
+	int getRow() const { return row; }
+	int getCol() const { return col; }
 	virtual void draw(int indent = 0) const = 0;
 };
 
@@ -32,12 +34,15 @@ public:
 	{
 		children.push_back(child);
 	}
+	int getRowCount() const { return rowCount; }
+	int getColCount() const { return colCount; }
+	const vector<WindowBase*>& getChildren() const { return children; }
 	void draw(int indent = 0) const override 
 	{
 		cout<<string(indent, ' ')<<"[Window] ID: "<<id
 			<<" Size: "<<rowCount<<"x"<<colCount
 			<<" Pos: ("<<row<<","<<col<<")\n";
-		for(auto ch : children)
+		for(WindowBase* ch : children)
 		{
 			ch->draw(indent+2);
 		}
@@ -87,77 +92,154 @@ public:
 class WindowManager {
 private:
 	vector<WindowBase*> elements;
+	Window* mainWindow = nullptr;
 	WindowBase* findById(const string& id)
 	{
-		for(auto e : elements)
+		for(WindowBase* e : elements)
 		{
 			if(e->getId() == id) return e;
 		}
 		return nullptr;
 	}
+	bool isCellAvailable(Window* parent, int r, int c)
+	{
+		if(!parent)
+		{
+			cerr<<"Parent window is null!\n";
+			return false;
+		}
+		if(r<0 || c<0 || r>=parent->getRowCount() || c>=parent->getColCount())
+		{
+			cerr<<"Cell ("<<r<<","<<c<<") is out of window "<<parent->getId()<<"!\n";
+			return false;
+		}
+		for(WindowBase* child : parent->getChildren())
+		{
+			if(child->getRow() == r && child->getCol() == c)
+			{
+				cerr<<"Cell ("<<r<<","<<c<<") is already occupied by Id: "<<child->getId()<<endl;
+				return false;
+			}
+		}
+		return true;
+	}
 	
 public:
 	~WindowManager()
 	{
-		for(auto e : elements) delete e;
+		for(WindowBase* e : elements) delete e;
 	}
 	void addWindow(string id, int rows, int cols, string parentId="-1", int row=-1, int col=-1)
 	{
-		WindowBase* parent = nullptr;
-		if(parentId != "-1") parent = findById(parentId);
+		if(findById(id) != nullptr)
+		{
+			cerr<<"Element with ID "<<id<<" already exists!\n";
+			return;
+		}
+		if(parentId == "-1")
+		{
+			if(mainWindow != nullptr)
+			{
+				cerr<<"Main window already exists!\n";
+				return;
+			}
+			mainWindow = new Window(id, rows, cols, nullptr, row, col);
+			elements.push_back(mainWindow);
+			return;
+		}
+		WindowBase* parent = findById(parentId);
+		if(!parent)
+		{
+			cerr<<"Parent window not found!\n";
+			return;
+		}
+		Window* pw = dynamic_cast<Window*>(parent);
+		if(!pw)
+		{
+			cerr<<"Parent is not a window!\n";
+			return;
+		}
+		if(!isCellAvailable(pw, row, col)) return;
 		Window* win = new Window(id, rows, cols, parent, row, col);
 		elements.push_back(win);
-		if(parent)
-		{
-			auto pw = dynamic_cast<Window*>(parent);
-			if(pw) pw->addChild(win);
-		}
+		pw->addChild(win);
 	}
 	void addText(string id, string text, string parentId, int row, int col)
 	{
-		auto parent = findById(parentId);
+		if(findById(id) != nullptr)
+		{
+			cerr<<"Element with ID "<<id<<"already exists!\n";
+			return;
+		}
+		WindowBase* parent = findById(parentId);
 		if(!parent)
 		{
 			cerr<<"Parent window not found!\n";
 			return;
 		}
+		Window* pw = dynamic_cast<Window*>(parent);
+		if(!pw)
+		{
+			cout<<"Parent is not a window!\n";
+			return;
+		}
+		if(!isCellAvailable(pw, row, col)) return;
 		Text* t = new Text(id, text, parent, row, col);
 		elements.push_back(t);
-		auto pw = dynamic_cast<Window*>(parent);
-		if(pw) pw->addChild(t);
+		pw->addChild(t);
 	}
 	void addTable(string id, int rows, int cols, string parentId, int row, int col)
 	{
-		auto parent = findById(parentId);
+		if(findById(id) != nullptr)
+		{
+			cerr<<"Element with ID "<<id<<"already exists!\n";
+			return;
+		}
+		WindowBase* parent = findById(parentId);
 		if(!parent)
 		{
 			cerr<<"Parent window not found!\n";
 			return;
 		}
+		Window* pw = dynamic_cast<Window*>(parent);
+		if(!pw)
+		{
+			cerr<<"Parent is not a window!\n";
+			return;
+		}
+		if(!isCellAvailable(pw, row, col)) return;
 		Table* tbl = new Table(id, rows, cols, parent, row, col);
 		elements.push_back(tbl);
-		auto pw = dynamic_cast<Window*>(parent);
-		if(pw) pw->addChild(tbl);
+		pw->addChild(tbl);
 	}
 	void addButton(string id, string text, string parentId, int row, int col)
 	{
-		auto parent = findById(parentId);
+		if(findById(id) != nullptr)
+		{
+			cerr<<"Element with ID "<<id<<"already exists!\n";
+			return;
+		}
+		WindowBase* parent = findById(parentId);
 		if(!parent)
 		{
 			cerr<<"Parent window not found!\n";
 			return;
 		}
+		Window* pw = dynamic_cast<Window*>(parent);
+		if(!pw)
+		{
+			cerr<<"Parent is not a window!\n";
+			return;
+		}
+		if(!isCellAvailable(pw, row, col)) return;
 		Button* btn = new Button(id, text, parent, row, col);
 		elements.push_back(btn);
-		auto pw = dynamic_cast<Window*>(parent);
-		if(pw) pw->addChild(btn);
+		pw->addChild(btn);
 	}
 	void drawAll()
 	{
-		for(auto e : elements)
-		{
-			if(!e->getParent()) e->draw();
-		}
+		if(mainWindow) mainWindow->draw();
+		else cerr<<"No main window to draw!\n";
 	}
 };
 
